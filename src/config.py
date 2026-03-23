aa5bfe772eef5ad3c1c59b86ecef62e52d87e69b
Senpaka <egorkazakov01012007@mail.ru>
2026-03-23 18:23:10 +0300

    refactor: Изменение конфигов на более лучшие

--- src/config.py
+++ src/config.py
@@ -4,27 +4,32 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List
 
+ROOT_DIR = Path(__file__).parent.parent
+
 @dataclass
 class Config:
 
-    data_path: Path = Path("data/data.csv")
-    model_path: Path = Path("model/model.pth")
-    log_path: Path = Path("log")
+    data_path: Path = ROOT_DIR / "data" / "data.csv"
+    model_path: Path = ROOT_DIR / "models" / "churn_model_final.pkl"
+    log_path: Path = ROOT_DIR / "log"
 
     scale_pos_weight: float = 3.9051667756703727
     threshold: float = 0.4
-    n_estimators: int = 100
+    n_estimators: int = 300
+    early_stopping_rounds: int = 20
     max_depth: int = 6
     learning_rate: float = 0.1
     random_state: int = 42
     test_size: float = 0.25
+    verbosity: int = 0
+    eval_metric: str = 'logloss'
 
     numerical_features: List[str] = field(default_factory=lambda: [
         'CreditScore', 'Balance', 'EstimatedSalary',
         'Tenure', 'NumOfProducts', 'Satisfaction Score', 'Point Earned'
     ])
     categorical_features: List[str] = field(default_factory=lambda: [
-        'Gender', 'Card Type', 'Geography'
+        'Gender', 'Card Type', 'Geography', 'Age_Group'
     ])
     columns_to_drop: List[str] = field(default_factory=lambda: [
         'RowNumber', 'CustomerId', 'Surname'
@@ -33,6 +38,6 @@ class Config:
     age_bins: List[str] = field(default_factory=lambda: [18, 30, 50, 65, 100])
     age_labels: List[str] = field(default_factory=lambda: ["18-29", "30-49", "50-64", "65+"])
 
-    pre_train_drop_features: List[str] = field(default_factory=lambda: ["Exited", "Geography_Readable", "Complain", "Age"])
+    pre_train_drop_features: List[str] = field(default_factory=lambda: ["Complain", "Age"])
 
 config = Config()
